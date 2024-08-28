f078cd11de8ba4963b2bc431cf4def6d4f78ccc8
denrzv <voofaa@gmail.com>
2024-08-28 12:20:21 +0300

    feat(task3): added replicas

--- mongo-sharding-repl/scripts/mongo-init.sh
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -15,7 +15,6 @@ rs.initiate({
     { _id: 2, host: "configSrv3:27019" }
   ]
 })
-exit()
 EOF
 
 sleep 5
@@ -24,24 +23,26 @@ sleep 5
 # Инициализация шардов
 ###
 
-docker compose exec -T shard1 mongosh --port 27020 --quiet <<EOF &
+docker compose exec -T shard1_1 mongosh --port 27020 --quiet <<EOF &
 rs.initiate({
   _id: "shard1",
   members: [
-    { _id: 0, host: "shard1:27020" }
+    { _id: 0, host: "shard1_1:27020" },
+    { _id: 1, host: "shard1_2:27120" },
+    { _id: 2, host: "shard1_3:27220" }
   ]
 })
-exit()
 EOF
 
-docker compose exec -T shard2 mongosh --port 27021 --quiet <<EOF &
+docker compose exec -T shard2_1 mongosh --port 27021 --quiet <<EOF &
 rs.initiate({
   _id: "shard2",
   members: [
-    { _id: 0, host: "shard2:27021" }
+    { _id: 0, host: "shard2_1:27021" },
+    { _id: 1, host: "shard2_2:27121" },
+    { _id: 2, host: "shard2_3:27221" }
   ]
 })
-exit()
 EOF
 
 wait
@@ -52,16 +53,16 @@ sleep 10
 # Добавляем шарды в кластер и инициализируем БД
 ###
 
-docker compose exec -T mongos_router mongosh --port 27022 --quiet <<EOF
-sh.addShard("shard1/shard1:27020")
-sh.addShard("shard2/shard2:27021")
+docker compose exec -T mongos_router1 mongosh --port 27022 --quiet <<EOF
+sh.addShard("shard1/shard1_1:27020")
+sh.addShard("shard2/shard2_1:27021")
 sh.enableSharding("somedb")
 sh.shardCollection("somedb.helloDoc", {"name" : "hashed"})
-var bulk = db.helloDoc.initializeUnorderedBulkOp();
-for(var i = 0; i < 1000; i++) {
-  bulk.insert({age:i, name:"ly"+i});
-}
-bulk.execute();
+use somedb
+for(var i = 0; i < 1000; i++) db.helloDoc.insert({age:i, name:"ly"+i})
 db.helloDoc.countDocuments()
-exit()
+EOF
+
+docker-compose exec -T mongos_router2 mongosh --port 27023  --quiet <<EOF
+db.adminCommand({ getShardMap: 1 })
 EOF
